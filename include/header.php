cec7d7080d9d2dbe599430e8ec2d946b3446e6cf
Johana Gonzalez <gonzalez.johana.7721@gmail.com>
2022-05-30 19:02:55 -0300

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html><!--Para indicar que es un documento html 5-->
+<html>
+    <head>
+        <meta charset="UTF-8"><!--Para indicar que es un documento en español-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Agostina Bossio</title>
+        <!--Bootstrap 4.2-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" 
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <!-- Font Awesome 5 (para los iconos)-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" 
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    </head>
+    <body>
+        <!-- scripts-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" 
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" 
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container">
+                <a href="menu.php" class="navbar-brand">Inicio</a>
+                <a href="productos.php" class="navbar-brand">Productos</a>
+                <a href="empleados.php" class="navbar-brand">Vendedores</a>
+                <a href="clientes.php" class="navbar-brand">Clientes</a>
+                <a href="proveedores.php" class="navbar-brand">Proveedores</a>
+                <a href="registroventas.php" class="navbar-brand">Ventas</a>
+                <a href="mercaderia.php" class="navbar-brand">Mercaderia</a>
+                <a href="sucursales.php" class="navbar-brand">Sucursales</a>
+            </div>
+        </nav>
+            
